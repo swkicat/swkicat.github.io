28a3e85e4a285ea7676d662c373ded47585f84af
swkicat <siwonkim0@naver.com>
2024-05-02 14:11:03 +0900

    :recycle: "edit intro"

--- gatsby-site-meta-data.ts
+++ gatsby-site-meta-data.ts
@@ -1,6 +1,6 @@
 export default {
-  title: `개발자 스티치`,
-  description: `개발자 스티치`,
+  title: `swkicat`,
+  description: `swkicat`,
   language: `ko`, // `ko`, `en` => currently support versions for Korean and English
   siteUrl: `https://github.com/swkicat`,
   ogImage: `/og-image.png`, // Path to your in the 'static' folder
@@ -19,8 +19,8 @@ export default {
       bachelorDegree: '',
     },
     social: {
-      github: `https://github.com/taewooyo`,
-      linkedIn: `https://www.linkedin.com/in/태우-남-449403226/`,
+      github: `https://github.com/swkicat`,
+      linkedIn: `https://www.linkedin.com/in/siwon-kim-02a792178/`,
       resume: ``,
     },
   },
